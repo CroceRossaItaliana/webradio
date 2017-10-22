f9b1e66418dc3d45bd283dc2bb94c50dbe87382d
Sgorauss <31952922+Sgorauss@users.noreply.github.com>
2017-10-22 21:38:12 +0200

    Update .htInfo.php

--- .htInfo.php
+++ .htInfo.php
@@ -5,7 +5,7 @@ $PAGE_TITLE = "Info";
 $PAGE_CONTENT = "
 	<h1>Info</h1>
 	<h3>T.L.C.ensus 2010</h3>
-	<div>Programma realizzato da Marco Avidano (VdS CRI Moncalvo - AT) per la Croce Rossa Italiana, servizio IV informatica.</div>
+	<div>Programma realizzato da Marco Avidano (Volontario CRI Moncalvo - AT) per la Croce Rossa Italiana.</div>
 	<div>Per inserimenti di nuovi apparati o antenne, segnalazione di malfunzionamenti o errori, scrivere a <a href='mailto:telecomunicazioni@cri.it'>telecomunicazioni@cri.it</a></div>
 	<div>Per scaricare il manuale d'uso del programma <a href='Manuale_TLCensus.pdf'>clicca qui</a></div>
 ";
