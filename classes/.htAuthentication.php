21c68c16daacb74b730ad99342042f85bf64f96a
Wiz <wizzent@users.noreply.github.com>
2018-03-20 16:20:20 +0100

    Add files via upload

--- classes/.htAuthentication.php
+++ classes/.htAuthentication.php
@@ -2,7 +2,7 @@
 /**
  * @package CRI Web Radio
  * @author WizLab.it
- * @version 20180315.085
+ * @version 20180320.086
  */
 
 /*===========================================================================
@@ -29,12 +29,12 @@ class Authentication {
     "Home" => array("title"=>"Home", "types"=>"all"),
     "AlboOperatoriTLC" => array("title"=>"Albo Operatori TLC", "types"=>"login"),
   	 "Utenti" => array("title"=>"Utenti", "types"=>array(1)),
-    "Radio" => array("title"=>"Radio", "types"=>array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)),
-    "Ripetitori" => array("title"=>"Ripetitori", "types"=>array(0, 1, 2, 3, 4, 6, 7, 8, 9, 10)),
-    "Mappa" => array("title"=>"Mappa", "types"=>array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)),
-    "Docs" => array("title"=>"Documenti", "types"=>array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10), "hideFromMenu"=>true),
-    "RipetitoriSezioni" => array("title"=>"Ripetitori Sezioni", "types"=>array(0, 1, 2, 3, 4, 6, 7, 8, 9, 10), "hideFromMenu"=>true),
-    "DocumentazioneMinistero" => array("title"=>"Documentazione Ministero", "types"=>array(1, 2)),
+    "Radio" => array("title"=>"Radio", "types"=>array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)),
+    "Ripetitori" => array("title"=>"Ripetitori", "types"=>array(0, 1, 2, 3, 4, 6, 7, 8, 9, 10, 11)),
+    "Mappa" => array("title"=>"Mappa", "types"=>array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)),
+    "Docs" => array("title"=>"Documenti", "types"=>array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), "hideFromMenu"=>true),
+    "RipetitoriSezioni" => array("title"=>"Ripetitori Sezioni", "types"=>array(0, 1, 2, 3, 4, 6, 7, 8, 9, 10, 11), "hideFromMenu"=>true),
+    "DocumentazioneMinistero" => array("title"=>"Documentazione Ministero", "types"=>array(1, 2, 11)),
     "ModelliRadio" => array("title"=>"Modelli Radio", "types"=>array(1)),
     "ModelliRipetitori" => array("title"=>"Modelli Ripetitori", "types"=>array(1)),
     "ModelliAntenne" => array("title"=>"Modelli Antenne", "types"=>array(1)),
@@ -45,9 +45,9 @@ class Authentication {
     "ProfiloUtente" => array("title"=>"Profilo Utente", "types"=>"login"),
     "Messaggi" => array("title"=>"Messaggi", "types"=>array(1)),
     "Strumenti" => array("title"=>"Strumenti", "types"=>array(1)),
-    "ElencoSedi" => array("title"=>"Elenco Sedi", "types"=>array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)),
-    "SegnalazioneInterferenze" => array("title"=>"Segnalazione interferenze", "types"=>array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)),
-    "CodiciCentralizzatiEmergenze" => array("title"=>"Codici centralizzati ed emergenze", "types"=>array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)),
+    "ElencoSedi" => array("title"=>"Elenco Sedi", "types"=>array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)),
+    "SegnalazioneInterferenze" => array("title"=>"Segnalazione interferenze", "types"=>array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)),
+    "CodiciCentralizzatiEmergenze" => array("title"=>"Codici centralizzati ed emergenze", "types"=>array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)),
     //"IscrizioneCorsoFormatoriTLC" => array("title"=>"Iscrizione Corso Formatori TLC", "types"=>array(1)),
   	 "Cerca" => array("types"=>"login", "hideFromMenu"=>true),
     "CambiaPassword" => array("title"=>"Cambia password", "types"=>"login"),
