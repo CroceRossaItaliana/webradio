c6c2a000b06254258738364f643dc3b2ec0bc8f2
Wiz <wizzent@users.noreply.github.com>
2018-04-05 14:57:57 +0200

    Add files via upload

--- .htCodiciCentralizzatiEmergenze.php
+++ .htCodiciCentralizzatiEmergenze.php
@@ -1,6 +1,32 @@
 <?php
-//20110627.002
-logMessage("Scaricato Elenco Codici 99 e Centralizzati");
-header("Location: /Codici_99_E_Centralizzati.pdf");
-die();
+/**
+ * @package CRI Web Radio
+ * @author WizLab.it
+ * @version 20180405.003
+ */
+
+$FILENAME = "/filez/docs/CodiciCentralizzatiEdEmergenze.pdf";
+
+$PAGE_TITLE = "Codici Centralizzati ed Emergenze";
+
+logMessage("Scaricato Elenco Codici Centralizzati ed Emergenze");
+
+$PAGE_CONTENT = "<h1>Codici Centralizzati ed Emergenze</h1>
+<h3>Codici Centralizzati ed Emergenze</h3>
+<div>Codici Centralizzati ed Emergenze.</div>
+<div><a href='" . $FILENAME . "' target='_blank'>Scarica Codici Centralizzati ed Emergenze</a></div>";
+
+//Gestione file
+if(in_array($LOGIN->getUserType()["id"], array(1, 2))) {
+  $PAGE_CONTENT .= "<h1 style='margin:50px 0 10px;'>Gestione file (only admin)</h1>";
+  if($_FILES["doc"] && ($_FILES["doc"]["error"] == 0) && ($_FILES["doc"]["type"] == "application/pdf")) {
+    logMessage("Modulo Codici Centralizzati ed Emergenze sostituito");
+    move_uploaded_file($_FILES["doc"]["tmp_name"], $_SERVER["DOCUMENT_ROOT"] . $FILENAME);
+    $PAGE_CONTENT .= BasicTable::showMessageBox("Documento sostituito");
+  }
+  $PAGE_CONTENT .= "<form method='post' action='" . $_SERVER["SCRIPT_NAME"] . "?cmd=" . $_REQUEST["cmd"] . "' enctype='multipart/form-data' style='margin-top:10px;'>
+    Nuovo documento: <input type='file' name='doc' />
+    <input type='submit' value='Salva documento' />
+  </form>";
+}
 ?>
--- .htRichiestaRipetitoriTemporanei.php
+++ .htRichiestaRipetitoriTemporanei.php
@@ -5,22 +5,22 @@
  * @version 20180405.004
  */
 
-$FILENAME = "/filez/docs/SegnalazioneInterferenze.pdf";
+$FILENAME = "/filez/docs/RichiestaRipetitoriTemporanei.pdf";
 
-$PAGE_TITLE = "Segnalazione Interferenze";
+$PAGE_TITLE = "Richiesta Ripetitori Temporanei";
 
-logMessage("Aperta pagina download Modulo Segnalazione Interferenze");
+logMessage("Aperta pagina Richiesta Ripetitori Temporanei");
 
-$PAGE_CONTENT = "<h1>Segnalazione Interferenze</h1>
-<h3>Modulo per Segnalazione Interferenze</h3>
-<div>Modulo segnalazione interferenze da compilare, stampare ed inviare ai recapiti dell'Ufficio Nazionale Radiocomunicazioni indicati sul modulo stesso.</div>
-<div><a href='" . $FILENAME . "' target='_blank'>Scarica Modulo per Segnalazione Interferenze</a></div>";
+$PAGE_CONTENT = "<h1>Richiesta Ripetitori Temporanei</h1>
+<h3>Modulo per Richiesta Ripetitori Temporanei</h3>
+<div>Modulo Richiesta Ripetitori Temporanei da compilare, stampare ed inviare ai recapiti dell'Ufficio Nazionale Radiocomunicazioni indicati sul modulo stesso.</div>
+<div><a href='" . $FILENAME . "' target='_blank'>Scarica Modulo per Richiesta Ripetitori Temporanei</a></div>";
 
 //Gestione file
 if(in_array($LOGIN->getUserType()["id"], array(1, 2))) {
   $PAGE_CONTENT .= "<h1 style='margin:50px 0 10px;'>Gestione file (only admin)</h1>";
   if($_FILES["doc"] && ($_FILES["doc"]["error"] == 0) && ($_FILES["doc"]["type"] == "application/pdf")) {
-    logMessage("Modulo Segnalazione Interferenze sostituito");
+    logMessage("Modulo Richiesta Ripetitori Temporanei sostituito");
     move_uploaded_file($_FILES["doc"]["tmp_name"], $_SERVER["DOCUMENT_ROOT"] . $FILENAME);
     $PAGE_CONTENT .= BasicTable::showMessageBox("Documento sostituito");
   }
